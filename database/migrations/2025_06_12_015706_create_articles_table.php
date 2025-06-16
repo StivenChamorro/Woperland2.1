2679b7443f0c5ecfcc64db8faa310be7c1335ed6
StivenChamorro <stivenchamorro493@gmail.com>
2025-06-15 22:20:11 -0500

    first commit

---
+++ database/migrations/2025_06_12_015706_create_articles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->double('price');
+            $table->string('avatar');  //este campo almacena la url de la imagen
+            $table->string('type')->default('normal');
+            $table->foreignId('store_id')->references('id')->on('stores')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
---
+++ database/migrations/2025_06_12_023121_create_childrens_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('childrens', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('lastname');
+            $table->date('birthdate');
+            $table->string('nickname');
+            $table->string('relation');
+            $table->string('avatar')->nullable(); //este campo alamcena la url de la imagen
+            $table->enum('gender', ['male', 'female', 'other']);
+            $table->integer('diamonds')->default(0);
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->text('about')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('childrens');
+    }
+};
